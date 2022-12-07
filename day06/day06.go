e1383342469f2117ab27da9076edbf63cae4d7d3
Rob Elliott <relliott@bastiansolutions.com>
2022-12-06 17:22:07 -0700

    added code for day06

---
+++ day06/day06.go
@@ -0,0 +1,66 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+)
+
+func check(e error) {
+	if e != nil {
+		panic(e)
+	}
+}
+
+// Readln returns a single line (without the ending \n)
+// from the input buffered reader.
+// An error is returned iff there is an error with the
+// buffered reader.
+func Readln(r *bufio.Reader) (string, error) {
+	var (
+		isPrefix       = true
+		err      error = nil
+		line, ln []byte
+	)
+	for isPrefix && err == nil {
+		line, isPrefix, err = r.ReadLine()
+		ln = append(ln, line...)
+	}
+	return string(ln), err
+}
+
+func checkMarker(s string) bool {
+	for i:= 0; i < len(s); i++{
+		for j := i+1; j < len(s); j++{
+			if s[i] == s[j] {
+				return false
+			}
+		}
+	}
+	return true
+}
+
+func findKeyPos(s string)(numChars int){
+	for i:=0; i < (len(s)-14); i++ {
+		if checkMarker(s[i:i+14]) {
+			numChars = i
+			break
+		}
+	}
+	return numChars
+}
+
+func main() {
+
+	f, err := os.Open("/home/relliott/aoc_2022/day06/input_day06.txt")
+	check(err)
+	r := bufio.NewReader(f)
+	var numChars int = 0
+	var s, e = Readln(r)
+	for e == nil {
+		numChars = findKeyPos(s) + 14
+		s, e = Readln(r)
+	}
+	fmt.Println("First key appears after: ", numChars, " characters")
+
+}
